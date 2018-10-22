239dfc5b29ebcb64134c40b8b74090c14e8681bb
soltanoff <piccadillable@gmail.com>
2018-10-22 21:43:32 +0300

    upd settings.py

--- systemd_watcher/settings.py
+++ systemd_watcher/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
+    'watcher.apps.WatcherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,6 +60,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',

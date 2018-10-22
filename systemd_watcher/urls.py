68c4db50b8b3a289b42d562bb257ec16cba23a86
soltanoff <piccadillable@gmail.com>
2018-10-22 21:43:56 +0300

    add main url patterns

--- systemd_watcher/urls.py
+++ systemd_watcher/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'', include('watcher.urls')),
+    url(r'^accounts/', include('account.urls')),
+    url(r'^manage/', admin.site.urls),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

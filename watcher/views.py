d0f0d939a00abb70a92b7e930465ca2f7725cf07
soltanoff <piccadillable@gmail.com>
2018-10-30 23:33:01 +0300

    sort service list

--- watcher/views.py
+++ watcher/views.py
@@ -1,3 +1,5 @@
+import operator
+
 from django.views.generic import TemplateView
 
 from service_monitor import ServiceMonitor
@@ -7,5 +9,5 @@ class ServiceView(TemplateView):
     template_name = "watcher/index.html"
 
     def get_context_data(self, **kwargs):
-        kwargs['services'] = ServiceMonitor.get_service_list()
+        kwargs['services'] = sorted(ServiceMonitor.iter_service_list(), key=operator.itemgetter('name'))
         return super(ServiceView, self).get_context_data(**kwargs)

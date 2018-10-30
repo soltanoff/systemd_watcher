f0293cb846fb47b77f83392e1434f87aa0df28a8
soltanoff <piccadillable@gmail.com>
2018-10-30 23:26:11 +0300

    implemented the `get_context_data` method

--- watcher/views.py
+++ watcher/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
+from service_monitor import ServiceMonitor
+
 
 class ServiceView(TemplateView):
     template_name = "watcher/index.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['services'] = ServiceMonitor.get_service_list()
+        return super(ServiceView, self).get_context_data(**kwargs)

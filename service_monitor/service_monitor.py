ccf620f9a07551fc747d81550a144fc67b9a585a
soltanoff <piccadillable@gmail.com>
2018-10-30 23:51:24 +0300

    [#1] add get methods, use regexp for extract data + move ServiceMonitor from test.py
    (cherry picked from commit cc15a75b65662d8e6f690a96196b7a4fdadc20a7)
    
    # Conflicts:
    #	service_monitor/service_monitor.py

--- service_monitor/service_monitor.py
+++ service_monitor/service_monitor.py
@@ -25,8 +25,14 @@ class ServiceMonitor:
     def __exec(cmd):
         return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
 
+    def __get_failed_services(self):
+        return self.__exec(SYSTEMCTL_COMMANDS.FAILED)
+
     def __get_service_list(self):
         return re.findall(self.__SERVICE_PATTERN, self.get_status_all())
+
+    def get_status_all(self):
+        return self.__exec(SYSTEMCTL_COMMANDS.STATUS_ALL)
 
     def __get_service_status(self, service_name):
         return self.__exec(SYSTEMCTL_COMMANDS.STATUS.format(service_name))
@@ -43,14 +49,15 @@ class ServiceMonitor:
         for status in filter(bool, map(self.__get_service_status, self.__get_service_list())):
             yield self.extract_data(status)
 
-    def get_failed_services(self):
-        return self.__exec(SYSTEMCTL_COMMANDS.FAILED)
+    def iter_failed_list(self):
+        for status in filter(bool, map(self.__get_service_status, self.__get_service_list())):
+            yield self.extract_data(status)
+
+    def get_failed_list(self):
+        return list(self.iter_failed_list())
 
     def get_service_list(self):
         return list(self.iter_service_list())
-
-    def get_status_all(self):
-        return self.__exec(SYSTEMCTL_COMMANDS.STATUS_ALL)
 
     def get_service_status(self, service_name):
         return self.extract_data(self.__get_service_status(service_name))
@@ -67,4 +74,4 @@ class ServiceMonitor:
 
 if __name__ == '__main__':
     monitor = ServiceMonitor()
-    print(monitor.get_failed_list())
+    print(monitor.get_service_list())

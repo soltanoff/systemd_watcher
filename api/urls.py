55b3fd6a26ee23578539f0558826a08dd6224198
soltanoff <piccadillable@gmail.com>
2018-11-08 21:58:29 +0300

    extend api urls & views

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from api import views
 from api.apps import ApiConfig
-from api.views import ListServices, ApiRoot
 
 app_name = ApiConfig.name
 urlpatterns = format_suffix_patterns([
-    path('', ApiRoot.as_view()),
-    path('services/', ListServices.as_view(), name='services'),
+    path('', views.ApiRoot.as_view()),
+    path('active_services/', views.ListServices.as_view(), name='active_services'),
+    path('failed_services/', views.FailedServices.as_view(), name='failed_services'),
 ])
--- api/views.py
+++ api/views.py
@@ -9,7 +9,8 @@ from service_monitor import ServiceMonitor
 class ApiRoot(APIView):
     def get(self, request, format=None):
         return Response({
-            'services': reverse('api:services', request=request, format=format),
+            'Active services': reverse('api:active_services', request=request, format=format),
+            'Failed services': reverse('api:failed_services', request=request, format=format),
         })
 
 
@@ -18,3 +19,10 @@ class ListServices(APIView):
 
     def get(self, request, format=None):
         return Response(ServiceMonitor.get_service_list())
+
+
+class FailedServices(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get(self, request, format=None):
+        return Response(ServiceMonitor.get_failed_services())

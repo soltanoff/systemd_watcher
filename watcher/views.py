f83bb0f68b859ae05a6075bde9953164a1cb1b79
soltanoff <piccadillable@gmail.com>
2019-01-04 15:57:11 +0300

    clear main webapp view

--- watcher/views.py
+++ watcher/views.py
@@ -1,13 +1,5 @@
-import operator
-
 from django.views.generic import TemplateView
-
-from service_monitor import ServiceMonitor
 
 
 class ServiceView(TemplateView):
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        kwargs['services'] = sorted(ServiceMonitor.iter_service_list(), key=operator.itemgetter('name'))
-        return super(ServiceView, self).get_context_data(**kwargs)

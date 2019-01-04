8cbe9c17b42a25f1bf21424ea5703347374b0ce0
soltanoff <piccadillable@gmail.com>
2019-01-04 15:58:01 +0300

    added new Vue.js method

--- watcher/static/js/main.js
+++ watcher/static/js/main.js
@@ -1,15 +1,26 @@
 new Vue({
-  el: '#starting',
+  el: '#app',
   delimiters: ['${','}'],
   data: {
     loading: false,
     preview: false,
     message: null,
     failedServicesData: '',
+    services: [],
   },
   mounted: function() {
+    this.getServices();
   },
   methods: {
+    getServices: function() {
+      this.$http.get(`/api/v1/services/`)
+          .then((response) => {
+            this.services = response.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+    },
     showFailedServices: function() {
       this.$http.get(`/api/v1/services/failed/`)
           .then((response) => {

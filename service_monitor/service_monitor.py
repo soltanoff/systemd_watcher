7f49332a3804aa161bdbf7640b8d749ed64dac0c
soltanoff <piccadillable@gmail.com>
2019-01-12 22:37:31 +0300

    refactoring + added new command

--- service_monitor/service_monitor.py
+++ service_monitor/service_monitor.py
@@ -4,18 +4,19 @@ import subprocess
 
 
 class SYSTEMCTL_COMMANDS:
+    ENABLED_SERVICES = "systemctl list-unit-files | grep enabled | grep -Po \'^.*\.service\'"
+    FAILED_SERVICES = 'systemctl --failed'
     STATUS_ALL = 'systemctl status'
     STATUS = 'systemctl status {0}'
     START = 'systemctl start {0}'
     RESTART = 'systemctl restart {0}'
     STOP = 'systemctl stop {0}'
-    FAILED = 'systemctl --failed'
 
 
 class ServiceMonitor:
-    __SERVICE_PATTERN = re.compile('─(.*\.service)')
+    __SERVICE_PATTERN = re.compile("─(.*\.service)")
     __NAME_PATTERN = re.compile(r"^●?\s?.*\s-\s(.*)\n")
-    __SERVICE_NAME_PATTERN = re.compile(r"^●?\s?(.*)\s-")
+    __SERVICE_NAME_PATTERN = re.compile(r"^●?\s?(.*)\s-?")
     __STATUS_PATTERN = re.compile(r"Active:\s(.*)\n")
     __DESCRIPTION_PATTERN = re.compile(
         r"(^\s+Loaded:\s.*\n|^\s+Process:\s.*\n|^\s+Main PID:\s.*\n|^\s+CGroup:\s.*(\n.*)+\n\n)",
@@ -26,50 +27,44 @@ class ServiceMonitor:
     def __exec(cmd):
         return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
 
-    def __get_failed_services(self):
-        return self.__exec(SYSTEMCTL_COMMANDS.FAILED)
-
-    def __get_service_list(self):
-        return re.findall(self.__SERVICE_PATTERN, self.get_status_all())
-
-    def get_status_all(self):
-        return self.__exec(SYSTEMCTL_COMMANDS.STATUS_ALL)
-
-    def __get_service_status(self, service_name):
-        return self.__exec(SYSTEMCTL_COMMANDS.STATUS.format(service_name))
-
-    def extract_data(self, data):
-        return {
-            'name': re.findall(self.__NAME_PATTERN, data)[0],
-            'service_name': re.findall(self.__SERVICE_NAME_PATTERN, data)[0],
-            'status': re.findall(self.__STATUS_PATTERN, data)[0],
-            'description': ''.join(map(lambda x: x[0], re.findall(self.__DESCRIPTION_PATTERN, data)))
-        }
-
-    def iter_service_list(self):
-        for status in filter(bool, map(self.__get_service_status, self.__get_service_list())):
-            yield self.extract_data(status)
-
-    def get_failed_services(self):
-        return self.__get_failed_services()
-
-    def get_service_list(self):
-        return list(sorted(self.iter_service_list(), key=operator.itemgetter('name')))
-
-    def get_service_status(self, service_name):
-        return self.extract_data(self.__get_service_status(service_name))
-
-    def set_start_service(self, service_name):
+    def start_service(self, service_name):
         return self.__exec(SYSTEMCTL_COMMANDS.START.format(service_name))
 
-    def set_restart_service(self, service_name):
+    def restart_service(self, service_name):
         return self.__exec(SYSTEMCTL_COMMANDS.RESTART.format(service_name))
 
-    def set_stop_service(self, service_name):
+    def stop_service(self, service_name):
         return self.__exec(SYSTEMCTL_COMMANDS.STOP.format(service_name))
+
+    def get_service_status(self, service):
+        status = self.__exec(SYSTEMCTL_COMMANDS.STATUS.format(service))
+        if status:
+            name = re.findall(self.__NAME_PATTERN, status)
+            service_name = re.findall(self.__SERVICE_NAME_PATTERN, status)[0]
+            return {
+                'name': name[0] if name else service_name,
+                'service_name': service_name,
+                'status': re.findall(self.__STATUS_PATTERN, status)[0],
+                'description': ''.join(map(lambda x: x[0], re.findall(self.__DESCRIPTION_PATTERN, status)))
+            }
+        return None
+
+    def __extract_data(self, services_name):
+        return list(sorted(filter(bool, map(self.get_service_status, services_name)), key=operator.itemgetter('name')))
+
+    def get_enabled_services(self):
+        return self.__extract_data(self.__exec(SYSTEMCTL_COMMANDS.ENABLED_SERVICES).split('\n')[:-1:])
+
+    def get_active_services(self):
+        return self.__extract_data(re.findall(self.__SERVICE_PATTERN, self.__exec(SYSTEMCTL_COMMANDS.STATUS_ALL)))
+
+    def get_failed_services(self):
+        return self.__exec(SYSTEMCTL_COMMANDS.FAILED_SERVICES)
 
 
 if __name__ == '__main__':
     monitor = ServiceMonitor()
-    print(monitor.get_service_list())
+    print(monitor.get_service_status('mysql'))
+    print(monitor.get_active_services())
+    print(monitor.get_enabled_services())
     print(monitor.get_failed_services())

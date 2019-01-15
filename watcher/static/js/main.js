66b00926f4c835ab0030b4d640e6b3354b643b92
soltanoff <piccadillable@gmail.com>
2019-01-15 17:51:32 +0300

    reformat code + added `pickedServices` v-model watcher

--- watcher/static/js/main.js
+++ watcher/static/js/main.js
@@ -1,87 +1,98 @@
 new Vue({
-  el: '#app',
-  delimiters: ['${','}'],
-  data: {
-    loading: false,
-    preview: false,
-    message: null,
-    serviceStatus: '',
-    failedServicesData: '',
-    services: [],
-  },
-  mounted: function() {
-    this.getServices();
-  },
-  methods: {
-    getServices: function() {
-      this.$http.get('/api/v1/services/')
-          .then((response) => {
-            this.services = response.data;
-          })
-          .catch((err) => {
-            console.log(err);
-          })
+    el: '#app',
+    delimiters: ['${','}'],
+    data: {
+        loading: false,
+        preview: false,
+        pickedServices: 'enabled',
+        message: null,
+        serviceStatus: '',
+        failedServicesData: '',
+        services: [],
     },
-    getActiveServices: function() {
-      this.$http.get('/api/v1/services/active/')
-          .then((response) => {
-            this.services = response.data;
-          })
-          .catch((err) => {
-            console.log(err);
-          })
+    mounted: function() {
+        this.getServices();
     },
-    showFailedServices: function() {
-      this.$http.get('/api/v1/services/failed/')
-          .then((response) => {
-            this.failedServicesData = response.data;
-            $("#showFailedServices").modal('show');
-          })
-          .catch((err) => {
-            console.log(err);
-          })
+    watch: {
+        pickedServices: function(value) {
+            if (value == 'enabled') {
+                this.getServices();
+            }
+            else {
+                this.getActiveServices();
+            }
+        }
     },
-    startService: function(service) {
-      this.$http.post('/api/v1/service/start/' + service + '/')
-          .then((response) => {
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" started successful!";
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-          .catch((err) => {
-            console.log(err);
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" not started!<br>Error: " + err;
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-    },
-    restartService: function(service) {
-      this.$http.post('/api/v1/service/restart/' + service + '/')
-          .then((response) => {
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" restarted successful!";
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-          .catch((err) => {
-            console.log(err);
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" not restarted!<br>Error: " + err;
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-    },
-    stopService: function(service) {
-      this.$http.post('/api/v1/service/stop/' + service + '/')
-          .then((response) => {
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" stopped!";
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-          .catch((err) => {
-            console.log(err);
-            this.serviceStatus = "Service \"<b>" + service + "</b>\" not stopped!<br>Error: " + err;
-            this.getServices();
-            $("#showServiceStatus").modal('show');
-          })
-    },
-  }
+    methods: {
+        getServices: function() {
+            this.$http.get('/api/v1/services/')
+            .then((response) => {
+                this.services = response.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        getActiveServices: function() {
+            this.$http.get('/api/v1/services/active/')
+            .then((response) => {
+                this.services = response.data;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        showFailedServices: function() {
+            this.$http.get('/api/v1/services/failed/')
+            .then((response) => {
+                this.failedServicesData = response.data;
+                $("#showFailedServices").modal('show');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },
+        startService: function(service) {
+            this.$http.post('/api/v1/service/start/' + service + '/')
+            .then((response) => {
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" started successful!";
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+            .catch((err) => {
+                console.log(err);
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" not started!<br>Error: " + err;
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+        },
+        restartService: function(service) {
+            this.$http.post('/api/v1/service/restart/' + service + '/')
+            .then((response) => {
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" restarted successful!";
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+            .catch((err) => {
+                console.log(err);
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" not restarted!<br>Error: " + err;
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+        },
+        stopService: function(service) {
+            this.$http.post('/api/v1/service/stop/' + service + '/')
+            .then((response) => {
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" stopped!";
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+            .catch((err) => {
+                console.log(err);
+                this.serviceStatus = "Service \"<b>" + service + "</b>\" not stopped!<br>Error: " + err;
+                this.getServices();
+                $("#showServiceStatus").modal('show');
+            })
+        },
+    }
 });

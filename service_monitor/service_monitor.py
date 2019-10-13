41f18122fa75d3d888afd74b2166472b907defb4
soltanoff <piccadillable@gmail.com>
2019-10-13 16:31:59 +0300

    Moved ServiceMonitor to dbus interface, make ServiceMonitor as singleton, updated api/views.py

--- service_monitor/service_monitor.py
+++ service_monitor/service_monitor.py
@@ -1,83 +1,159 @@
 import operator
-import re
-import subprocess
+
+import dbus
 
 
-class SystemctlCommands:
-    ENABLED_SERVICES = "systemctl list-unit-files | grep enabled | grep -Po \'^.*\.service\'"
-    FAILED_SERVICES = 'systemctl --failed'
-    STATUS_ALL = 'systemctl status'
-    STATUS = 'systemctl status {0}'
-    START = 'systemctl start {0}'
-    RESTART = 'systemctl restart {0}'
-    STOP = 'systemctl stop {0}'
+def catch_dbus_exception(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except dbus.exceptions.DBusException as error:
+            print(error)
+            return None
+
+    return wrapper
 
 
-class ServiceMonitor:
-    __SERVICE_PATTERN = re.compile("─(.*\.service)")
-    __NAME_PATTERN = re.compile(r"^●?\s?.*\s-\s(.*)\n")
-    __SERVICE_NAME_PATTERN = re.compile(r"^●?\s?(.*\.service)\s-?")
-    __STATUS_PATTERN = re.compile(r"Active:\s(.*)\n")
-    __DESCRIPTION_PATTERN = re.compile(
-        r"(^\s+Loaded:\s.*\n|^\s+Process:\s.*\n|^\s+Main PID:\s.*\n|^\s+CGroup:\s.*(\n.*)+\n\n)",
-        re.MULTILINE
-    )
+class ServiceMonitor(object):
 
-    @staticmethod
-    def __exec(cmd):
-        return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+    BUS_NAME = "org.freedesktop.systemd1"
+    OBJECT_PATH = "/org/freedesktop/systemd1"
 
-    @classmethod
-    def start_service(cls, service_name):
-        return cls.__exec(SystemctlCommands.START.format(service_name))
+    DBUS_MANAGER_INTERFACE = "org.freedesktop.systemd1.Manager"
+    DBUS_PROPERTIES_INTERFACE = "org.freedesktop.DBus.Properties"
 
-    @classmethod
-    def restart_service(cls, service_name):
-        return cls.__exec(SystemctlCommands.RESTART.format(service_name))
+    UNIT_INTERFACE = "org.freedesktop.systemd1.Unit"
+    SERVICE_UNIT_INTERFACE = "org.freedesktop.systemd1.Service"
 
-    @classmethod
-    def stop_service(cls, service_name):
-        return cls.__exec(SystemctlCommands.STOP.format(service_name))
+    __instance = None
 
-    @classmethod
-    def get_service_status(cls, service):
-        status = cls.__exec(SystemctlCommands.STATUS.format(service))
-        if status:
-            name = re.findall(cls.__NAME_PATTERN, status)
-            service_name = re.findall(cls.__SERVICE_NAME_PATTERN, status)[0]
-            return {
-                'name': name[0] if name else service_name,
-                'service_name': service_name,
-                'status': re.findall(cls.__STATUS_PATTERN, status)[0],
-                'description': ''.join(map(lambda x: x[0], re.findall(cls.__DESCRIPTION_PATTERN, status)))
-            }
-        return None
+    def __init__(self):
+        self.__bus = dbus.SystemBus()
 
-    @classmethod
-    def __extract_data(cls, service_names):
-        service_info_map = {x['name']: x for x in filter(bool, map(cls.get_service_status, service_names))}
-        return list(sorted(service_info_map.values(), key=operator.itemgetter('name')))
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(ServiceMonitor, cls).__new__(cls)
+        return cls.__instance
 
-    @classmethod
-    def get_enabled_services(cls):
-        return cls.__extract_data(cls.__exec(SystemctlCommands.ENABLED_SERVICES).split('\n')[:-1:])
+    @property
+    def _interface(self):
+        return dbus.Interface(self.__bus.get_object(self.BUS_NAME, self.OBJECT_PATH), self.DBUS_MANAGER_INTERFACE)
 
-    @classmethod
-    def get_active_services(cls):
-        return cls.__extract_data(re.findall(cls.__SERVICE_PATTERN, cls.__exec(SystemctlCommands.STATUS_ALL)))
+    def _get_unit_properties_by_interface(self, unit_name, unit_interface):
+        obj = self.__bus.get_object(self.BUS_NAME, self._interface.LoadUnit(unit_name))
+        properties_interface = dbus.Interface(obj, self.DBUS_PROPERTIES_INTERFACE)
+        return properties_interface.GetAll(unit_interface)
 
-    @classmethod
-    def get_inactive_services(cls):
-        return list(filter(lambda x: 'inactive' in x['status'], cls.get_enabled_services()))
+    def _iter_services(self, status=None):
+        for dbus_service_info in self._interface.ListUnits():
+            service_name = dbus_service_info[0]
+            service_status = dbus_service_info[3]
+            service_active_stage = dbus_service_info[4]
+            if (status is None or status == service_status) \
+                    and '.service' in service_name \
+                    and '@' not in service_name \
+                    and not service_name.startswith('systemd') \
+                    and service_active_stage != 'exited':
+                yield self.get_service_status(service_name)
 
-    @classmethod
-    def get_failed_services(cls):
-        return cls.__exec(SystemctlCommands.FAILED_SERVICES)
+    @catch_dbus_exception
+    def start_service(self, service_name, mode="replace"):
+        self._interface.StartUnit(service_name, mode)
+
+    @catch_dbus_exception
+    def restart_service(self, service_name, mode="replace"):
+        self._interface.RestartUnit(service_name, mode)
+
+    @catch_dbus_exception
+    def stop_service(self, service_name, mode="replace"):
+        self._interface.StopUnit(service_name, mode)
+
+    @catch_dbus_exception
+    def get_service_status(self, service_name):
+        service_property = self._get_unit_properties_by_interface(service_name, self.SERVICE_UNIT_INTERFACE)
+        unit_property = self._get_unit_properties_by_interface(service_name, self.UNIT_INTERFACE)
+
+        unit_file_state = unit_property.get('UnitFilePreset')
+        service_name = str(unit_property['Id'])
+        description = str(unit_property['Description'])
+        status = "Active: {activeState} ({subState})".format(
+            activeState=unit_property['ActiveState'],
+            subState=unit_property['SubState'],
+        )
+        service_info = "● {id} - {description}\n" \
+                       "Loaded: {loadState} ({fragmentPath}; {unitFileState}; vendor preset; {unitFilePreset})\n" \
+                       "{status}\n" \
+                       "Main PID: {pid}\n" \
+                       "Tasks: {tasksCurrent}\n" \
+                       "Memory: {memoryCurrent}M\n" \
+                       "CPU: {CPUUsageNSec}s\n" \
+                       "CGroup: {controlGroup}\n".format(
+            id=service_name,
+            description=description,
+            loadState=unit_property['LoadState'],
+            fragmentPath=unit_property['FragmentPath'],
+            unitFileState=unit_property['UnitFileState'],
+            unitFilePreset=unit_property['UnitFilePreset'],
+            status=status,
+            pid=service_property['MainPID'],
+            tasksCurrent=service_property['TasksCurrent'],
+            memoryCurrent=service_property['MemoryCurrent'] / 1024.0 / 1024.0,
+            CPUUsageNSec=service_property['CPUUsageNSec'] / 1000000.0,
+            controlGroup=service_property['ControlGroup'],
+        )
+
+        exec_start = service_property['ExecStart']
+        if exec_start:
+            service_info += "Exec start: {0}\n".format(' '.join(exec_start[0][1]))
+
+        exec_start_post = service_property['ExecStartPost']
+        if exec_start_post:
+            service_info += "Exec start post: {0}\n".format(' '.join(exec_start_post[0][1]))
+
+        exec_start_pre = service_property['ExecStartPre']
+        if exec_start_pre:
+            service_info += "Exec start pre: {0}\n".format(' '.join(exec_start_pre[0][1]))
+
+        exec_stop = service_property['ExecStop']
+        if exec_stop:
+            service_info += "Exec stop: {0}\n".format(' '.join(exec_stop[0][1]))
+
+        failure_action = service_property['FailureAction']
+        if failure_action:
+            service_info += "Failure action: {0}\n".format(failure_action)
+
+        exec_main_status = service_property['ExecMainStatus']
+        if exec_main_status:
+            service_info += "Exec main status: {0}\n".format(exec_main_status)
+
+        return {
+            'name': description if description else service_name,
+            'service_name': service_name,
+            'status': status,
+            'unit_file_state': str(unit_file_state) if unit_file_state else None,
+            'description': service_info,
+        }
+
+    @catch_dbus_exception
+    def get_enabled_services(self):
+        return list(sorted(self._iter_services(), key=operator.itemgetter('name')))
+
+    @catch_dbus_exception
+    def get_active_services(self):
+        return list(sorted(self._iter_services(status='active'), key=operator.itemgetter('name')))
+
+    @catch_dbus_exception
+    def get_inactive_services(self):
+        return list(sorted(self._iter_services(status='inactive'), key=operator.itemgetter('name')))
+
+    @catch_dbus_exception
+    def get_failed_services(self):
+        return list(sorted(self._iter_services(status='failed'), key=operator.itemgetter('name')))
 
 
 if __name__ == '__main__':
     monitor = ServiceMonitor()
-    print(monitor.get_service_status('mysql'))
+    print(monitor.get_service_status('mysql.service'))
     print(monitor.get_active_services())
     print(monitor.get_inactive_services())
     print(monitor.get_enabled_services())

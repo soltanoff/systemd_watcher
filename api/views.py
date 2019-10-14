e5ec9b484cba599867b926e9ed2a7b97b39c5f42
soltanoff <piccadillable@gmail.com>
2019-10-14 16:30:19 +0300

    [#6] logic for favorite service saving, created API views: FavoriteServices, ManageFavoriteServices

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
+from http import HTTPStatus
+
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 from service_monitor import ServiceMonitor
+from watcher.models import FavoriteServiceModel
 
 
 # TODO: soltanoff: use authentication.TokenAuthentication
@@ -16,7 +19,30 @@ class ApiRoot(APIView):
         return Response({
             'Services': reverse('api:services', request=request, format=format),
             'Failed services': reverse('api:failed_services', request=request, format=format),
+            'Inactive services': reverse('api:inactive_services', request=request, format=format),
+            'Favorite services': reverse('api:favorite_services', request=request, format=format),
         })
+
+
+class StartService(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def post(self, request, service_name, format=None):
+        return Response(ServiceMonitor().restart_service(service_name))
+
+
+class StopService(APIView):
+    permission_classes = (permissions.IsAdminUser,)
+
+    def post(self, request, service_name, format=None):
+        return Response(ServiceMonitor().stop_service(service_name))
+
+
+class ServiceStatus(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, service_name, format=None):
+        return Response(ServiceMonitor().get_service_status(service_name))
 
 
 class EnabledServices(APIView):
@@ -47,22 +73,29 @@ class FailedServices(APIView):
         return Response(ServiceMonitor().get_failed_services())
 
 
-class ServiceStatus(APIView):
+class FavoriteServices(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
-    def get(self, request, service_name, format=None):
-        return Response(ServiceMonitor().get_service_status(service_name))
+    def get(self, request, format=None):
+        result = []
+        monitor = ServiceMonitor()
+        for record in FavoriteServiceModel.objects.filter(user=request.user):
+            service_status = monitor.get_service_status(record.name)
+            if service_status:
+                result.append(service_status)
+
+        return Response(result)
 
 
-class StartService(APIView):
-    permission_classes = (permissions.IsAdminUser,)
+class ManageFavoriteServices(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
 
     def post(self, request, service_name, format=None):
-        return Response(ServiceMonitor().restart_service(service_name))
+        record = FavoriteServiceModel.objects.get(user=request.user, name=service_name)
+        if record:
+            record.delete()
+        else:
+            record = FavoriteServiceModel(user=request.user, name=service_name)
+            record.save()
 
-
-class StopService(APIView):
-    permission_classes = (permissions.IsAdminUser,)
-
-    def post(self, request, service_name, format=None):
-        return Response(ServiceMonitor().stop_service(service_name))
+        return Response(status=HTTPStatus.OK)
